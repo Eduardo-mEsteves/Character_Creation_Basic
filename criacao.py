2b8fbefbb8a2bab686982c6b06d6a94356172e80
Eduardo-mEsteves <eduardofrancamesteves@gmail.com>
2025-02-06 01:33:11 -0300

    Create criacao.py

---
+++ criacao.py
@@ -0,0 +1,37 @@
+import time
+import random
+from os import system as sys; sys('clear') 
+
+user_info = []
+
+print("Crie seu personagem!!!")
+time.sleep(3)
+sys('clear')
+user_info.append(input("Seu nick:"))
+sys('clear')
+
+print("Os dados estão decidindo seu HP...")
+time.sleep(3)
+
+n1 = random.randint (1,6)
+n2 = random.randint (1,6)
+n3 = (n1 + n2) * 10
+print (f"Sua vida: {n3}")
+user_info.append(n3)
+time.sleep(2)
+sys('clear')
+
+print ("Os dados estão decidindo seu ATK...")
+time.sleep(3)
+
+user_info.append(random.randint(1, 6) * 10) 
+print(f"Seu ATK: {user_info[2]}") 
+time.sleep(3)
+
+sys('clear')
+
+
+
+print(f"Nick: {user_info[0]} \nHP: {user_info[1]} \nATK: {user_info[2]}")
+
+print("\n Por enquanto é só isso, obrigado por testar! :) ")
